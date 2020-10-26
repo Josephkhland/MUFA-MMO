fe085b1874bb6da0c7ad75c5019b04ee719926a2
Josephkhland <Josephkhland@gmail.com>
2020-10-27 01:00:23 +0200

    Edited the command

--- Players/battle.py
+++ Players/battle.py
@@ -13,7 +13,7 @@ class Battle_Commands(commands.Cog):
         self.bot = bot
     
     @commands.command(name='attack')
-    async def attack_cool(self, ctx, *args):
+    async def attack_target(self, ctx, *args):
         if not character.checkRegistration(str(ctx.author.id)):
             return await ctx.send("You are not registered. Please register by using the command `!register`")
         if not await character.playabilityCheck(ctx, str(ctx.author.id)):
@@ -25,6 +25,12 @@ class Battle_Commands(commands.Cog):
         node = pCharac.getInstance()
         if not isinstance(node, db.Battle): 
             return await ctx.send("You can only use this command in a Battle.")
+        #Doesn't work for PvP -> Have to come up with something
+        targetted_monster = getFactionMember(int(args[0]),1)
+        if targetted_monster == None:
+            return await ctx.send("There is no such target in this Instance.")
+        message_to_display =mb.attack(battler,targetted_monster,0,targetted_monster, targetted_monster.getCharacter().name)
+        print(message_to_display)
         await ctx.send("YOU DID A VERY POWERFUL ATTACK!")
 
 def setup(bot):
